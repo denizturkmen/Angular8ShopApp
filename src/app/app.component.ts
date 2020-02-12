2deedc9c82f8e9a5af7afdcd1458ed161a1c2962
denizturkmen <turkmen_deniz@hotmail.com>
2020-02-12 15:01:35 +0200

    Cart
    Sepet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-
   selector: 'root',
-  template: '<shop></shop>'
+  template: "<router-outlet></router-outlet>"
 })
-export class AppComponent { }
+export class AppComponent { 
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
 import { AppComponent } from './app.component';
-import { ShopModule } from './Shop/shop.module';
-
-
+import { ShopModule } from './shop/shop.module';
+import { RouterModule } from '@angular/router';
+import { ShopComponent } from './shop/shop.component';
+import { CartDetailComponent } from './shop/cart-detail/cart-detail.component';
+import { CheckoutComponent } from './shop/checkout/checkout.component';
 
 @NgModule({
   declarations: [
@@ -11,7 +14,13 @@ import { ShopModule } from './Shop/shop.module';
   ],
   imports: [
     BrowserModule,
-    ShopModule
+    ShopModule,
+    RouterModule.forRoot([
+      { path: 'shop', component: ShopComponent},
+      { path: 'cart', component: CartDetailComponent},
+      { path: 'checkout', component: CheckoutComponent},
+      { path: '**', redirectTo: "/shop"}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
